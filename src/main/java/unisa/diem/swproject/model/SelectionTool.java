5faf9fd6eee88b7b93a566fa430b6d1a96f27bff
Amer1ko <105321460+Amer1ko@users.noreply.github.com>
2022-11-22 17:31:24 +0100

    task 5: implement the concrete tool for selection

---
+++ src/main/java/unisa/diem/swproject/model/SelectionTool.java
@@ -0,0 +1,48 @@
+package unisa.diem.swproject.model;
+
+public class SelectionTool implements Tool {
+    private ShapeManager shapeManager;
+    private Shape selected;
+
+    public SelectionTool(ShapeManager sm){
+        this.shapeManager = sm;
+        this.selected = null;
+    }
+
+    @Override
+    public void mouseDown(double mouseX, double mouseY) {
+        this.selected = this.shapeManager.getSelectedShape(mouseX, mouseY);
+    }
+
+    @Override
+    public void mouseUp(double mouseX, double mouseY) {
+
+    }
+
+    @Override
+    public void mouseDrag(double mouseX, double mouseY) {
+
+    }
+
+    @Override
+    public boolean isShapeTool() {
+        return true;
+    }
+
+    @Override
+    public int apply() {
+        this.shapeManager.setSelectedShape(this.selected);
+        return 0;
+    }
+
+    @Override
+    public int revert() {
+        this.shapeManager.setSelectedShape(null);
+        return 0;
+    }
+
+    @Override
+    public Shape getShape() {
+        return this.selected;
+    }
+}
