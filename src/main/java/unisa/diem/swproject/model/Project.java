4ca4b9bcaf22854e72a2bc233788c5a572fdbd55
Amer1ko <105321460+Amer1ko@users.noreply.github.com>
2022-11-22 16:47:37 +0100

    task 2: implement classes "Sheet" and "Project" and instantiate controller and view

--- src/main/java/unisa/diem/swproject/model/Project.java
+++ src/main/java/unisa/diem/swproject/model/Project.java
@@ -1,15 +1,30 @@
 package unisa.diem.swproject.model;
 
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+
 public class Project {
     private String name;
     private Iterable<Sheet> sheets;
     private CommandManager commandManager;
 
-    public void load(String filePath) {
-
+    public Project load(String fileName) {
+        Project p = null;
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+            p = (Project) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return p;
     }
 
-    public void save(String filePath) {
-
+    public void save(String filePath, Project p) {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            oos.writeObject(p);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
