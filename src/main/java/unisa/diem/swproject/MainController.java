4c488f4709401c965100bd8bfde0560f600c6736
Luigi Verolla <luverolla@gmx.it>
2022-11-24 17:58:10 +0100

    Task 14: create event listener for tools in MainController

--- src/main/java/unisa/diem/swproject/MainController.java
+++ src/main/java/unisa/diem/swproject/MainController.java
@@ -1,23 +1,28 @@
 package unisa.diem.swproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
-import unisa.diem.swproject.model.CommandManager;
-import unisa.diem.swproject.model.Project;
-import unisa.diem.swproject.model.Sheet;
-import unisa.diem.swproject.model.SheetFormat;
+import unisa.diem.swproject.model.*;
+import unisa.diem.swproject.model.tools.RectangleTool;
+
+import java.util.Map;
 
 public class MainController {
 
-    private Project project;
+    private final Project project;
 
-    private final CommandManager cm;
+    private final Map<String, Tool> toolMap;
 
     public MainController() {
-        cm = new CommandManager();
         project = new Project("Untitled");
         Sheet sheet = new Sheet("Sheet 1", SheetFormat.A4, null);
         project.addSheet(sheet);
+
+        toolMap = Map.ofEntries(
+            Map.entry("rectangle", new RectangleTool(sheet.shapeManager()))
+        );
     }
 
     @FXML
@@ -28,5 +33,36 @@ public class MainController {
         Sheet sheet = project.getSheet();
         canvasContainer.setContent(sheet);
         sheet.buildDrawingArea();
+
+        sheet.setOnMousePressed(e -> {
+            if (sheet.getCurrentTool() != null) {
+                sheet.getCurrentTool().mouseDown(e.getX(), e.getY());
+            }
+        });
+
+        sheet.setOnMouseReleased(e -> {
+            if (sheet.getCurrentTool() != null) {
+                sheet.getCurrentTool().mouseUp(e.getX(), e.getY());
+            }
+        });
+
+        sheet.setOnMouseMoved(e -> {
+            if (sheet.getCurrentTool() != null) {
+                sheet.getCurrentTool().mouseDrag(e.getX(), e.getY());
+            }
+        });
+    }
+
+    @FXML
+    public void selectTool(ActionEvent e) {
+        Node node = (Node) e.getSource();
+        String toolName = (String) node.getUserData();
+        Tool chosen = toolMap.get(toolName);
+
+        if(chosen.equals(project.getSheet().getCurrentTool())) {
+            project.getSheet().setCurrentTool(null);
+        } else {
+            project.getSheet().setCurrentTool(chosen);
+        }
     }
 }
