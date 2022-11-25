e93d27a7db9b5fdce847304b26b9159435eeedb0
Luigi Verolla <luverolla@gmx.it>
2022-11-25 12:00:46 +0100

    initialized structure for unit tests

---
+++ src/test/java/unisa/diem/swproject/CommandManagerTest.java
@@ -0,0 +1,12 @@
+package unisa.diem.swproject;
+
+import org.junit.jupiter.api.Test;
+import unisa.diem.swproject.model.CommandManager;
+
+public class CommandManagerTest {
+    CommandManager cm = new CommandManager();
+
+    @Test
+    public void addCommandTest() {}
+
+}
