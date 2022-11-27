f38b6244c7448940af0605078b73dae416ff8604
apoli34 <a.poli1@studenti.unisa.it>
2022-11-27 16:50:03 +0100

    Task 18

---
+++ src/main/java/unisa/diem/swproject/model/tools/ShapeTool.java
@@ -0,0 +1,8 @@
+package unisa.diem.swproject.model.tools;
+
+import javafx.scene.paint.Color;
+import unisa.diem.swproject.model.Tool;
+
+public interface ShapeTool extends Tool {
+    void setStrokeColor(Color strokeColor);
+}
